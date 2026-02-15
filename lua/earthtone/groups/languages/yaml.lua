76aff999f0fcaaf28bebc9291c8766d4de43f2a8
Dmytro Onypko <vaporif@proton.me>
2026-02-15 11:48:28 +0900

    languages

---
+++ lua/earthtone/groups/languages/yaml.lua
@@ -0,0 +1,8 @@
+return function(hi, c)
+  hi('@property.yaml', { fg = c.storm_blue })
+  hi('@string.yaml', { fg = c.moss })
+  hi('@number.yaml', { fg = c.warm_amber })
+  hi('@boolean.yaml', { fg = c.constant_warm })
+  hi('@constant.builtin.yaml', { fg = c.constant_warm })
+  hi('@keyword.yaml', { fg = c.keyword_warm })
+end
